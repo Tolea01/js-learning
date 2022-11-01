524ba11f3772ef7c8a9c224b67654c4152549811
Curbat Anatolie <anatoliecurbat@gmail.com>
2022-11-01 18:02:40 +0200

    Task #1 - conditional operator

---
+++ src/1.js
@@ -0,0 +1,33 @@
+let n = 3;
+
+switch (n) {
+    case 1:
+        console.log('One');
+        break;
+    case 2:
+        console.log('two');
+        break;
+    case 3:
+        console.log('three');
+        break;
+    case 4:
+        console.log('four');
+        break;
+    case 5:
+        console.log('five');
+        break;
+    case 6:
+        console.log('six');
+        break;
+    case 7:
+        console.log('seven');
+        break;
+    case 8:
+        console.log('eight');
+        break;
+    case 9:
+        console.log('nine');
+        break;
+    default:
+        console.log(n + ' ' + 'is not a digit');
+}
