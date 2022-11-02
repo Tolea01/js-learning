b7c498c30ad8bfada876d1f87ccf33f01a19c6d6
Curbat Anatolie <anatoliecurbat@gmail.com>
2022-11-02 02:26:56 +0200

    Task #3 - conditional operator

---
+++ src/3.js
@@ -0,0 +1,23 @@
+let a = 1500;
+let b = 200;
+let c = 100;
+
+if(a > b && a > c) {
+    console.log('Numarul mai mare este ' + a);
+}
+else if(b > a && b > c) {
+    console.log('Numarul mai mare este ' + b);
+}
+else if(c > a && c > b){
+    console.log('Numarul mai mare este ' + c);
+}
+
+if(a < b && a < c) {
+    console.log('Numarul mai mic este ' + a);
+}
+else if(b < a && b < c) {
+    console.log('Numarul mai mic este ' + b);
+}
+else if(c < a && c < b){
+    console.log('Numarul mai mic este ' + c);
+}
