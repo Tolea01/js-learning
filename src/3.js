16e3bccf561b44596058f4f81d681621eed6bd36
Curbat Anatolie <anatoliecurbat@gmail.com>
2022-11-02 02:28:37 +0200

    Task #3 - conditional operator V2

--- src/3.js
+++ src/3.js
@@ -2,22 +2,24 @@ let a = 1500;
 let b = 200;
 let c = 100;
 
-if(a > b && a > c) {
-    console.log('Numarul mai mare este ' + a);
-}
-else if(b > a && b > c) {
-    console.log('Numarul mai mare este ' + b);
-}
-else if(c > a && c > b){
-    console.log('Numarul mai mare este ' + c);
-}
+// if(a > b && a > c) {
+//     console.log('Numarul mai mare este ' + a);
+// }
+// else if(b > a && b > c) {
+//     console.log('Numarul mai mare este ' + b);
+// }
+// else if(c > a && c > b){
+//     console.log('Numarul mai mare este ' + c);
+// }
 
-if(a < b && a < c) {
-    console.log('Numarul mai mic este ' + a);
-}
-else if(b < a && b < c) {
-    console.log('Numarul mai mic este ' + b);
-}
-else if(c < a && c < b){
-    console.log('Numarul mai mic este ' + c);
-}
+// if(a < b && a < c) {
+//     console.log('Numarul mai mic este ' + a);
+// }
+// else if(b < a && b < c) {
+//     console.log('Numarul mai mic este ' + b);
+// }
+// else if(c < a && c < b){
+//     console.log('Numarul mai mic este ' + c);
+// }
+
+console.log('Numarul mai mare este: ' + Math.max(a, b, c) + '\n' + 'Numarul mai mic este: ' + Math.min(a, b, c));
