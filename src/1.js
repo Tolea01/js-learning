9286a01a1def5ac724b196c49ea51a6f14feb696
Curbat Anatolie <anatoliecurbat@gmail.com>
2022-11-05 16:23:07 +0200

    Task #1 - array display

---
+++ src/1.js
@@ -0,0 +1,41 @@
+let array = [12, 34, 'JavaScript', 5.3];
+
+// for(let i = 0; i < array.length; i++) {
+//     console.log(array[i]);
+// }
+
+
+// let i = 0;
+// while (i < array.length) {
+//     console.log(array[i])
+//     i++;
+//   }
+
+
+
+// let i = 0;
+// do {
+//     console.log(array[i]);
+//     i++;
+// }
+// while(i < array.length);
+
+
+
+// for(const i in array) {
+//     console.log(array[i]);
+// }
+
+
+
+// for(const item of array) {
+//     console.log(item);
+// }
+
+
+// array.forEach((elem) => console.log(elem));
+
+
+// array.map((elem) => console.log(elem));
+
+// I liked working with the forEach and map methods
