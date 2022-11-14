023f7cf232cfefb0a3089a34f0441790a4c20298
Curbat Anatolie <anatoliecurbat@gmail.com>
2022-11-14 02:32:10 +0200

    Task #7 - getters

---
+++ src/7.js
@@ -0,0 +1,37 @@
+let person = {
+    name: 'Person',
+    age: 123,
+    salary: 1542.33,
+    contacts: {
+        phone: 112,
+        email: 'email@domain.com',
+
+        get phone() {
+            return "phone is 112"
+        },
+
+        get email() {
+            return "email@domain.com"
+        }
+    },
+    adress: 'Moldova',
+
+    get name() {
+        return "this name is person"
+    },
+
+    get age() {
+        return 123 * 2
+    },
+
+    get salary() {
+        return Math.ceil(Math.random() * 100)
+    },
+
+    get adress() {
+        return 'adress is Moldova'
+    }
+}
+
+console.log(person);
+console.log(person.contacts.phone);
