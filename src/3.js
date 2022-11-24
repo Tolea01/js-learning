83a6faf5a6c005554c406732b322cb5ed49350d6
Curbat Anatolie <anatoliecurbat@gmail.com>
2022-11-24 03:50:37 +0200

    Task #3 - code review

--- src/3.js
+++ src/3.js
@@ -1,25 +1,4 @@
-let a = 1500;
-let b = 200;
-let c = 100;
-
-// if(a > b && a > c) {
-//     console.log('Numarul mai mare este ' + a);
-// }
-// else if(b > a && b > c) {
-//     console.log('Numarul mai mare este ' + b);
-// }
-// else if(c > a && c > b){
-//     console.log('Numarul mai mare este ' + c);
-// }
-
-// if(a < b && a < c) {
-//     console.log('Numarul mai mic este ' + a);
-// }
-// else if(b < a && b < c) {
-//     console.log('Numarul mai mic este ' + b);
-// }
-// else if(c < a && c < b){
-//     console.log('Numarul mai mic este ' + c);
-// }
-
-console.log('Numarul mai mare este: ' + Math.max(a, b, c) + '\n' + 'Numarul mai mic este: ' + Math.min(a, b, c));
+const a = 1500;
+const b = 200;
+const c = 100;
+console.log(`Numarul mai mare este: ${Math.max(a, b, c)}\nNumarul mai mic este: ${Math.min(a, b, c)}`);
