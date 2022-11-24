5bafc47be3de4a27109e0492e4a43ec72c6e37c8
Curbat Anatolie <anatoliecurbat@gmail.com>
2022-11-25 00:40:39 +0200

    Task #3 - code review

--- src/3.js
+++ src/3.js
@@ -1,6 +1,10 @@
-let styles = ['Джаз', 'Блюз'];
-styles.push('Рок-н-ролл');
-styles[Math.floor((styles.length - 1) / 2)] = "Классика";    /* ******* */
-console.log(styles.shift());
+const styles = ['Джаз', 'Блюз'];
+console.log(styles);
+styles.push('Рок-н-ролл')
+console.log(styles);
+styles.splice(Math.floor(styles.length / 2), 1, 'Классика')
+console.log(styles);
+styles.shift();
+console.log(styles);
 styles.unshift('Рэп', 'Регги')
-// console.log(styles);
+console.log(styles);
