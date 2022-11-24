e71b8f1c2d888d1cebc1ae40820d682301944c5e
Curbat Anatolie <anatoliecurbat@gmail.com>
2022-11-25 01:32:22 +0200

    Task #8 - code review

--- src/8.js
+++ src/8.js
@@ -1,28 +1,9 @@
 const colors = ['red', 'green', 'blue',];
-
-// function checkColor(color) {
-
-//     for (let elem in colors) {
-//         if (colors[elem] == color) {                                /* ********************************* */
-//             console.log(colors.indexOf(color));
-//         }
-//         // else{
-//         //     console.log(-1);
-//         // }
-//     }
-
-// }
-
-// checkColor('green');
-
-function checkColor(color) {
-    let result = colors.findIndex((elem) => {
+const checkColor = (color) => {
+    return colors.findIndex((elem) => {
         if (elem == color) {
             return elem;
-        }
-    })
-
-    console.log(result);
-}
-
-checkColor('green');
+        };
+    });
+};
+console.log(checkColor('blue'));
