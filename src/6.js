58f4a9d66a9dc6cba79c2fcc3d7169d7699cfad5
Curbat Anatolie <anatoliecurbat@gmail.com>
2022-11-25 01:20:55 +0200

    Task #6 - code review

--- src/6.js
+++ src/6.js
@@ -1,6 +1,6 @@
-function addElements (array, el1, el2, el3, el4) {
+const addElements = (array, el1, el2, el3, el4) => {
     array.splice(4, el1, el2, el3, el4);
     console.log(array);
-}
+};
 
-addElements([1, 2, 3, 4, 5, 6, 7, 8, 9], 0,0,0,0);
+addElements([1, 2, 3, 4, 5, 6, 7, 8, 9], 0, 0, 0, 0);
