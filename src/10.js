927ac08c44b635013f6ceb2af20a733b384f455e
Curbat Anatolie <anatoliecurbat@gmail.com>
2022-11-25 01:38:06 +0200

    Task #10 - code review

--- src/10.js
+++ src/10.js
@@ -1,10 +1,7 @@
-let array = [8, 7, 2, 2, 3, 4, 2, 5, 5, 6, 7, 8, 5, 9, 0, 1];
-
-function uniqueSorted() {
-    let sort = array.sort((a, b) => { return a - b });
-    let unique = Array.from(new Set(array));
+const array = [8, 7, 2, 2, 3, 4, 2, 5, 5, 6, 7, 8, 5, 9, 0, 1];
+const uniqueSorted = () => {
+    const sort = array.sort((a, b) => { return a - b });
+    const unique = Array.from(new Set(array));
     return sort, unique;
-
-}
-
+};
 console.log(uniqueSorted());
