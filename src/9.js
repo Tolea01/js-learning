c20d244a91bf5ae1aae5a09150fa948dc7761aae
Curbat Anatolie <anatoliecurbat@gmail.com>
2022-11-25 01:34:34 +0200

    Task #9 - code review

--- src/9.js
+++ src/9.js
@@ -1,7 +1,5 @@
-let array = [1, 'a', 2, null, 3, undefined, 4, {}, 5, 6, `word`, 7, 8, false, 9, 100];
-
-let result = array.filter((elem) => {
+const array = [1, 'a', 2, null, 3, undefined, 4, {}, 5, 6, `word`, 7, 8, false, 9, 100];
+const result = array.filter((elem) => {
     return Number(elem);
-})
-
+});
 console.log(result);
