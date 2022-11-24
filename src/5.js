2f5d06c21eec6334b61f621ce20cd26ece7961a0
Curbat Anatolie <anatoliecurbat@gmail.com>
2022-11-25 01:13:57 +0200

    Task #5 - code review

--- src/5.js
+++ src/5.js
@@ -1,12 +1,10 @@
-let arr = [1, 2, 3, 4];
+const arr = [1, 2, 3, 4];
 
-function convert(fn, array) {
-    let newArr = [];
-    array.map((elem) => {
-        newArr.push(fn(elem));                                    /* ******************* */
-    })
-    return newArr
+const convert = (fn, array) => {
+    const newArr = [];
+    array.forEach((elem) => newArr.push(fn(elem)));
+    return newArr;
 }
 
-function square(x) {return x*x};
+function square(x) { return x * x };
 console.log(convert(square, arr));
