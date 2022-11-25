c41a359923881283aefd0f5f677f32c2623c2613
Curbat Anatolie <anatoliecurbat@gmail.com>
2022-11-25 22:33:35 +0200

    Task #3 - code review

--- src/3.js
+++ src/3.js
@@ -1,18 +1,15 @@
-let menu = {
+const menu = {
     width: 200,
     height: 300,
     title: "My menu"
 };
 
-function multiplyNumeric(obj) {
-
+const multiplyNumeric = (obj) => {
     for (let key in obj) {
-        if (obj[key] == Number(obj[key])) {     /* typeof obj[key] == 'number' */
+        if (typeof obj[key] == 'number') {
             obj[key] *= 2;
-        }
-    }
-
-    console.log(obj);
-}
-
-multiplyNumeric(menu);
+        };
+    };
+    return obj;
+};
+console.log(multiplyNumeric(menu));
