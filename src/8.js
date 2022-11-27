ef013962e8b5527b4df1656fdeb8220530300b2a
Curbat Anatolie <anatoliecurbat@gmail.com>
2022-11-27 11:25:30 +0200

    Task #8 - code review

--- src/8.js
+++ src/8.js
@@ -8,9 +8,9 @@ const weekDays = {
     "Su": "Воскресенье",
 }
 
-let days = Object.keys(weekDays);
-let translateRom = ['Luni', 'Marti', 'Miercuri', 'Joi', 'Vineri', 'Sambata', 'Duminica',];
-let object = {};
+const days = Object.keys(weekDays);
+const translateRom = ['Luni', 'Marti', 'Miercuri', 'Joi', 'Vineri', 'Sambata', 'Duminica',];
+const object = {};
 
 for (let keys in days) {
     object[days[keys]] = translateRom[keys];
