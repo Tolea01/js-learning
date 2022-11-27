05ae4f7fcfa03a7ae9049cbd31a70bcc0d9c32c4
Curbat Anatolie <anatoliecurbat@gmail.com>
2022-11-27 11:11:00 +0200

    Task #11 - code review

--- src/11.js
+++ src/11.js
@@ -1,4 +1,4 @@
-let array = [
+const array = [
     {
         name: 'student1',
         marks: [8, 10, 2, 5, 4],
@@ -26,7 +26,7 @@ let array = [
 ];
 
 // 1.******************************************************************************************
-let result = array.map(elem => {
+const result = array.map((elem) => {
     return {
         name: elem.name,
         mark: elem.marks.reduce((prev, current) => prev + current) / elem.marks.length
@@ -38,40 +38,40 @@ let result = array.map(elem => {
 
 
 // 2.******************************************************************************************
-// let result2 = result.filter(item => item.mark < 5);
+// const result2 = result.filter((item) => item.mark < 5);
 // console.log(result2);
 // ************************************************************************************************
 
 
 // 3.******************************************************************************************
-// let max = result.reduce((prev, current) => {
+// const max = result.reduce((prev, current) => {
 //     if (prev.mark > current.mark) {
 //         return prev
-//     }
-//     return current
-// })
+//     };
+//     return current;
+// });
 
-// let min = result.reduce((prev, current) => {
+// const min = result.reduce((prev, current) => {
 //     if (prev.mark < current.mark) {
-//         return prev
-//     }
-//     return current
-// })
+//         return prev;
+//     };
+//     return current;
+// });
 // console.log('Max:', max);
 // console.log('Min:', min);
 // ************************************************************************************************
 
 
 // 4.******************************************************************************************
-// let result4 = result.sort((a, b) => b.mark - a.mark);
+// const result4 = result.sort((a, b) => b.mark - a.mark);
 // console.log(result4);
 // ************************************************************************************************
 
 
 // 5.******************************************************************************************
-// let studentsAverage = result.map(e => e.mark);
-// let sum = studentsAverage.reduce((prev, current) => prev + current, 0);
-// let classAverage = sum / studentsAverage.length;
-// let result5 = result.find(elem => elem.mark > classAverage)
+// const studentsAverage = result.map((e) => e.mark);
+// const sum = studentsAverage.reduce((prev, current) => prev + current, 0);
+// const classAverage = sum / studentsAverage.length;
+// const result5 = result.find(elem => elem.mark > classAverage);
 // console.log(result5);
 // ************************************************************************************************
