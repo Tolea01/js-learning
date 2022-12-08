8ef116dbbaa5cbe50a04855c2d1e087ea988cc35
Curbat Anatolie <anatoliecurbat@gmail.com>
2022-12-08 03:21:07 +0200

    Task #8 - working with time

---
+++ src/8.js
@@ -0,0 +1,17 @@
+document.querySelector('#start').addEventListener('click', () => {
+    const interval = setInterval(() => {
+        let hours = new Date().getHours();
+        const minutes = new Date().getMinutes();
+        const seconds = new Date().getSeconds();
+
+        hours < 10 ? hours = '0' + hours : hours;
+
+        document.querySelector('#red').textContent = hours;
+        document.querySelector('#green').textContent = ':' + minutes;
+        document.querySelector('#blue').textContent = ':' + seconds;
+    });
+
+    document.querySelector('#stop').addEventListener('click', () => {
+        clearInterval(interval);
+    })
+});
