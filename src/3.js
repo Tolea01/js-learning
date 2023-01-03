b96a14614c57d1eebc391e520f085131dd3ea0e4
Curbat Anatolie <anatoliecurbat@gmail.com>
2023-01-04 01:09:03 +0200

    Task #3 code review

--- src/3.js
+++ src/3.js
@@ -4,21 +4,17 @@ const c = 100;
 
 if (a > b && a > c) {
     console.log('Numarul mai mare este ' + a);
-}
-else if (b > a && b > c) {
+} else if (b > a && b > c) {
     console.log('Numarul mai mare este ' + b);
-}
-else if (c > a && c > b) {
+} else if (c > a && c > b) {
     console.log('Numarul mai mare este ' + c);
 }
 
 if (a < b && a < c) {
     console.log('Numarul mai mic este ' + a);
-}
-else if (b < a && b < c) {
+} else if (b < a && b < c) {
     console.log('Numarul mai mic este ' + b);
-}
-else if (c < a && c < b) {
+} else if (c < a && c < b) {
     console.log('Numarul mai mic este ' + c);
 }
 
