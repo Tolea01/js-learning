c51e3f943376ac7497ff4d91ca49df0388c3f6ac
Curbat Anatolie <anatoliecurbat@gmail.com>
2023-01-08 01:17:56 +0300

    Task #2 - document loading event

---
+++ src/1-4.js
@@ -0,0 +1,7 @@
+window.onload = () => {
+   document.body.style.background = 'linear-gradient(135deg, #e55d87 0%, #5fc3e4 100%)';
+}
+
+setTimeout(() => {
+   document.body.style.background = 'green';
+}, 3000);
