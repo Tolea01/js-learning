ec307570ccb8b47119f812ab5f7eb42a05e30df3
Curbat Anatolie <anatoliecurbat@gmail.com>
2023-01-08 22:05:28 +0300

    Task #4 - adding elements to DOM

--- src/1-4.js
+++ src/1-4.js
@@ -9,3 +9,33 @@ setTimeout(() => {
 document.querySelector('#buttonOrange').addEventListener('click', () => {
    document.body.style.background = 'orange';
 });
+
+const addItem = () => {
+   document.body.append(document.createElement('ol'));
+
+   const list = document.querySelector('ol');
+   const newItem = document.createElement('li');
+   const itemNumber = list.children.length + 1;
+
+   newItem.textContent = `Элемент ${itemNumber}`;
+
+   if (itemNumber % 2 == 0) {
+      newItem.style.backgroundColor = 'green';
+   } else {
+      newItem.style.backgroundColor = 'purple';
+   }
+
+   list.append(newItem);
+}
+
+setInterval(() => {
+   document.body.append(document.createElement('ol'));
+
+   const list = document.querySelector('ol');
+
+   if (list.children.length >= 20) {
+      return;
+   }
+
+   addItem();
+}, 2000);
--- src/1.js
+++
@@ -1,3 +0,0 @@
-window.onload = () => {
-   document.body.style.background = 'linear-gradient(135deg, #e55d87 0%, #5fc3e4 100%)';
-}
