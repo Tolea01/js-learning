e0a75ad18fffa7912438d631285d19a27f27387f
Curbat Anatolie <anatoliecurbat@gmail.com>
2023-01-10 00:39:48 +0300

    Task #6 - adding elements to DOM

---
+++ src/6.js
@@ -0,0 +1,23 @@
+const table = document.createElement('table');
+
+document.body.appendChild(table);
+
+table.style.cssText = 'width: 500px; height: 500px; border: 2px solid black';
+
+for (let i = 0; i < 8; i++) {
+   const row = document.createElement('tr');
+
+   for (let j = 0; j < 8; j++) {
+      const cell = document.createElement('td');
+
+      if ((i + j) % 2 === 0) {
+         cell.style.cssText = 'background-color: white; border: 2px solid black'
+      } else {
+         cell.style.backgroundColor = 'black';
+      }
+
+      row.appendChild(cell);
+   }
+
+   table.appendChild(row);
+}
