3d9199cb9734f22c30d3cf19c3f47afe74f7cfa8
Curbat Anatolie <anatoliecurbat@gmail.com>
2023-01-17 12:54:53 +0300

    code review

--- src/5.js
+++ src/5.js
@@ -2,8 +2,6 @@ function getPrimes(n) {
     const primes = [];
 
     for (let i = 2; i <= n; i++) {
-        let isPrime = true;
-
         for (let j = 2; j < i; j++) {
             if (i % j == 0) {
                 isPrime = false;
