3eb59bee3a26fe63c1cf101bf3f03ae151d0e962
Curbat Anatolie <anatoliecurbat@gmail.com>
2023-01-17 20:46:01 +0300

    Task#6 code review

--- src/6.js
+++ src/6.js
@@ -1,8 +1,12 @@
-const input = prompt('Introduceti anul, luna, data nasterii');
-const currentDate = new Date();
-const dateOfBirth = new Date(input);
-const days = Math.ceil(Math.abs(dateOfBirth.getTime() - currentDate.getTime()) / (1000 * 3600 * 24));
-const year = Math.floor(days / 365);
-const months = Math.floor(year * 12);
+const calculateAge = () => {
+   const input = prompt('Introduceti anul, luna, data nasterii');
+   const currentDate = new Date();
+   const dateOfBirth = new Date(input);
+   const days = Math.ceil(Math.abs(dateOfBirth.getTime() - currentDate.getTime()) / (1000 * 3600 * 24));
+   const year = Math.floor(days / 365);
+   const months = Math.floor(year * 12);
 
-console.log(`${year} ani\n${months} luni\n${days} zile`);
+   console.log(`${year} ani\n${months} luni\n${days} zile`);
+};
+
+calculateAge();
