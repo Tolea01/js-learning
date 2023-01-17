bc066ef5c8aa69cfcc2ab2e1d7704642745c06a6
Curbat Anatolie <anatoliecurbat@gmail.com>
2023-01-18 02:30:09 +0300

    Task#9 code review

--- src/9.js
+++ src/9.js
@@ -1,21 +1,38 @@
-document.querySelector('.timer-button').addEventListener('click', () => {
+const startButton = document.querySelector('#timer-button');
+const daysElement = document.querySelector('#days');
+const hoursElement = document.querySelector('#hours');
+const minutesElement = document.querySelector('#minutes');
+const secondsElement = document.querySelector('#seconds');
+
+const timer = () => {
     const inputDate = new Date(prompt('Introduceti anul.luna.data.ora:minute:secunde'));
-    const startButton = setInterval(() => {
+
+    if (!inputDate || inputDate.toString() === "Invalid Date") {
+        alert("Неверная дата ввода");
+        return;
+    }
+
+    const timerInterval = setInterval(() => {
         const now = new Date();
         const interval = inputDate - now;
-        const days = Math.floor(interval / 1000 / 60 / 60 / 24);
-        const hours = Math.floor(interval / 1000 / 60 / 60) % 24;
-        const minutes = Math.floor(interval / 1000 / 60) % 60;
-        const seconds = Math.floor(interval / 1000) % 60;
 
         if (interval <= 0) {
-            clearInterval(startButton);
+            clearInterval(timerInterval);
             alert('Время пришло');
         } else {
-            document.querySelector('#days').textContent = days;
-            document.querySelector('#hours').textContent = hours;
-            document.querySelector('#minutes').textContent = minutes;
-            document.querySelector('#seconds').textContent = seconds;
+            const days = Math.floor(interval / 1000 / 60 / 60 / 24);
+            const hours = Math.floor(interval / 1000 / 60 / 60) % 24;
+            const minutes = Math.floor(interval / 1000 / 60) % 60;
+            const seconds = Math.floor(interval / 1000) % 60;
+
+            daysElement.textContent = days;
+            hoursElement.textContent = hours;
+            minutesElement.textContent = minutes;
+            secondsElement.textContent = seconds;
         }
     }, 1000);
+};
+
+startButton.addEventListener('click', () => {
+    timer();
 });
