cc8b9fff1dc44300d2616ec0bb76a77d34ed2e99
Curbat Anatolie <anatoliecurbat@gmail.com>
2023-01-17 20:31:37 +0300

    Task#3 code review

--- src/3.js
+++ src/3.js
@@ -1,4 +1,6 @@
-// const getMonthName = (date, month, year) => new Date(year, month - 1, date).toLocaleString('en', {month: 'long'});
+// const getMonthName = (date, month, year) => {
+//     return new Date(year, month - 1, date).toLocaleString('en', { month: 'long' });
+// };
 
 // console.log(getMonthName(10, 10, 2009));
 
@@ -7,7 +9,7 @@ const getMonthName = (month) => {
     const monthList = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
     return monthList[month.getMonth()];
-}
+};
 
 console.log(getMonthName(new Date("10/11/2009")));
 console.log(getMonthName(new Date("11/13/2014")));
