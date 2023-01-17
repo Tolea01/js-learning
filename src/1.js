1e8b9922071eeeb45d4f7002163e66c562603799
Curbat Anatolie <anatoliecurbat@gmail.com>
2023-01-17 20:23:14 +0300

    Task #1 code-review

--- src/1.js
+++ src/1.js
@@ -1,9 +1,11 @@
-const corectDate = (date) => date < 10 ? date = '0' + date: date; 
+const corectDate = (date) => {
+    return date < 10 ? date = '0' + date : date;
+}
 
 const getCurrentDate = (separator) => {
-    
+
     const date = new Date();
-    return `${corectDate(date.getDate())}${separator}${corectDate(date.getMonth())}${separator}${corectDate(date.getFullYear())}`;
+    return `${corectDate(date.getDate())}${separator}${corectDate(date.getMonth() + 1)}${separator}${corectDate(date.getFullYear())}`;
 };
 
 console.log(getCurrentDate('/'));
