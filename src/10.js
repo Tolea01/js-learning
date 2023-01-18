f3f02150c61555dba89a9893ed2a6058efe6a2f7
Curbat Anatolie <anatoliecurbat@gmail.com>
2023-01-18 23:50:19 +0300

    Task#10 code review

--- src/10.js
+++ src/10.js
@@ -1,42 +1,38 @@
 const person1 = {
-    name: 'Person',
-    age: 1236,
-    salary: 1542.33,
-    contacts: {
-        phone: '112',
-        email: 'email@domain.com',
-    },
-    address: 'Moldova',
+  name: 'Person',
+  age: 1236,
+  salary: 1542.33,
+  contacts: {
+    phone: '112',
+    email: 'email@domain.com',
+  },
+  address: 'Moldova',
 
 };
 
-
 const person2 = {
-    name: 'Person2',
-    age: 123,
-    salary: 1542.33,
-    contacts: {
-        phone: '112',
-        email: 'email@ain.com',
-    },
-    address: 'Moldova',
+  name: 'Person2',
+  age: 123,
+  salary: 1542.33,
+  contacts: {
+    phone: '112',
+    email: 'email@ain.com',
+  },
+  address: 'Moldova',
 };
 
 const intersection = (firstObject, secondObject) => {
-    const result = {};
+  const result = {};
 
-    for (let key in firstObject) {
-        if (firstObject[key] == secondObject[key]) {
-            result[key] = firstObject[key];
-        } else if (typeof firstObject[key] == 'object' && typeof secondObject[key] == 'object') {
-            for (let i in firstObject[key]) {
-                if (firstObject[key][i] == secondObject[key][i]) {
-                    result[i] = firstObject[key][i];
-                }
-            }
-        }
-    };
-    return result;
+  for (const key of Object.keys(firstObject)) {
+    if (typeof firstObject[key] === 'object') {
+      result[key] = intersection(firstObject[key], secondObject[key]);
+    } else if (firstObject[key] === secondObject[key]) {
+      result[key] = firstObject[key];
+    }
+  }
+
+  return result;
 };
 
 console.log(intersection(person1, person2));
