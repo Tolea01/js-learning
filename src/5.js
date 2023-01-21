83f4d0b6364daf002845b082ef21e35f82fcf0f2
Curbat Anatolie <anatoliecurbat@gmail.com>
2023-01-22 00:09:20 +0200

    Task#5 code review

--- src/5.js
+++ src/5.js
@@ -1,44 +1,22 @@
-const json = JSON.stringify({
-    "name": "John",
-    "surname": "Kennedy",
-    "patronymic": "Fitzgerald",
-    "date-of-birth": "12.12.1980",
-    "contacts": {
-        "phone": "+37360583217",
-        "home-phone-number": "022398538",
-        "adress": "Balti",
-        "email": "kenedy1980@gmail.com"
-    },
-    "place-of-birth": "Balti",
-    "gender": "M",
-    "job": "driver",
-    "start-date": "14.12.2018",
-    "IDNP": "2005375938279",
-    "salary": "2000$",
-    "work-experience": "10 years",
-    "department": "drivers",
-    "work-schedule": {
-        "from": "8:00",
-        "to": "17:00"
+// https://jsoneditoronline.org/#left=cloud.11876c9373004f95bccd3b40a4efb8c6&right=local.wukafo
+
+const jsonObject = '{"name":"John","surname":"Kennedy","patronymic":"Fitzgerald","date-of-birth":"12.12.1980","contacts":{"phone":"+37360583217","home-phone-number":"022398538","adress":"Balti","email":"kenedy1980@gmail.com"},"place-of-birth":"Balti","gender":"M","job":"driver","start-date":"14.12.2018","IDNP":"2005375938279","salary":"2000$","work-experience":"10 years","department":"drivers","work-schedule":{"from":"8:00","to":"17:00"}}';
+
+const person = JSON.parse(jsonObject);
+
+const test = (person) => {
+  for (const [key, value] of Object.entries(person)) {
+    const element = document.querySelector(`#${key}`);
+
+    if (element) {
+      element.textContent = value;
     }
-});
+    if (typeof value === 'object') {
+      test(value);
+    }
+  }
+};
 
-const person = JSON.parse(json);
+test(person);
 
-document.querySelector('#name').textContent = `${person.name}`;
-document.querySelector('#surname').textContent = `${person.surname}`;
-document.querySelector('#patronymic').textContent = `${person.patronymic}`;
-document.querySelector('#date-of-birth').textContent = `${person['date-of-birth']}`;
-document.querySelector('#phone').textContent = `${person.contacts.phone}`;
-document.querySelector('#home-phone-number').textContent = `${person.contacts['home-phone-number']}`;
-document.querySelector('#adress').textContent = `${person.contacts.adress}`;
-document.querySelector('#email').textContent = `${person.contacts.email}`;
-document.querySelector('#place-of-birth').textContent = `${person['place-of-birth']}`;
-document.querySelector('#gender').textContent = `${person.gender}`;
-document.querySelector('#job').textContent = `${person.job}`;
-document.querySelector('#start-date').textContent = `${person['start-date']}`;
-document.querySelector('#IDNP').textContent = `${person.IDNP}`;
-document.querySelector('#salary').textContent = `${person.salary}`;
-document.querySelector('#work-experience').textContent = `${person['work-experience']}`;
-document.querySelector('#department').textContent = `${person.department}`;
-document.querySelector('#work-schedule').textContent = `${person['work-schedule'].from} - ${person['work-schedule'].to}`;
+document.querySelector('#schedule').textContent = `${person['work-schedule'].to} - ${person['work-schedule'].from}`;
