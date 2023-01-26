66241379443120c1b80a20d7e19578cd8be13b35
Curbat Anatolie <anatoliecurbat@gmail.com>
2023-01-26 11:41:28 +0200

    Task#5

---
+++ Task#5/script/script.js
@@ -0,0 +1,24 @@
+const plusButton = document.getElementById('plus-button');
+const minusButton = document.getElementById('minus-button');
+const input = document.getElementById('input');
+
+plusButton.addEventListener('click', () => {
+  if (input.value < 9) {
+    input.value++;
+  }
+  if (input.value === 9) {
+    plusButton.disabled = true;
+  }
+});
+
+minusButton.addEventListener('click', () => {
+  if (input.value > 0) {
+    input.value--;
+  }
+  if (input.value === 0) {
+    minusButton.disabled = true;
+  }
+});
+
+input.setAttribute('readonly', '');
+
