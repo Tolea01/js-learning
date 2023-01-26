44772201bdf0606d299c6ec090a1ea2a76fbe8d2
Curbat Anatolie <anatoliecurbat@gmail.com>
2023-01-26 21:23:56 +0300

    Task#5 code review

--- Task#5/script/script.js
+++ Task#5/script/script.js
@@ -2,23 +2,31 @@ const plusButton = document.getElementById('plus-button');
 const minusButton = document.getElementById('minus-button');
 const input = document.getElementById('input');
 
+input.setAttribute('readonly', '');
+
+const updatevalue = (button, operator) => {
+  if (button && operator) {
+    if (operator === '+') {
+      if (input.value < 9) {
+        input.value++;
+      }
+    } else if (operator === '-') {
+      if (input.value > 0) {
+        input.value--;
+      }
+    }
+    if (input.value === 9 || input.value === 0) {
+      button.disabled = true;
+    }
+  } else {
+    console.error('Enter all parameters in the "updateValue" function');
+  }
+}
+
 plusButton.addEventListener('click', () => {
-  if (input.value < 9) {
-    input.value++;
-  }
-  if (input.value === 9) {
-    plusButton.disabled = true;
-  }
+  updatevalue(plusButton, '+');
 });
 
 minusButton.addEventListener('click', () => {
-  if (input.value > 0) {
-    input.value--;
-  }
-  if (input.value === 0) {
-    minusButton.disabled = true;
-  }
+  updatevalue(minusButton, '-');
 });
-
-input.setAttribute('readonly', '');
-
