d3adab044d38bed1a7f78c5323e2cac61688f1b8
Tolea2001 <anatoliecurbat@gmail.com>
2023-01-29 01:48:56 +0300

    Task#18 code review

---
+++ Task#18/script/script.js
@@ -0,0 +1,65 @@
+const player = document.querySelector('#player');
+const player2 = document.querySelector('#player2');
+const viewport = document.querySelector('#viewport');
+
+const movePlayerWithKeyboard = (event) => {
+
+  switch (event.key) {
+
+    case 'ArrowUp':
+      player.style.top = player.offsetTop - 10 + "px";
+      break;
+
+    case 'ArrowDown':
+      player.style.top = player.offsetTop + 10 + "px";
+      break;
+
+    case 'ArrowLeft':
+      player.style.left = player.offsetLeft - 10 + "px";
+      break;
+
+    case 'ArrowRight':
+      player.style.left = player.offsetLeft + 10 + "px";
+      break;
+
+    default:
+      break;
+
+  }
+
+};
+
+const movePlayer2WithKeyboard = (event) => {
+
+  switch (event.key) {
+
+    case 'ArrowUp':
+      player2.offsetTop > viewport.offsetTop ? player2.style.top = player2.offsetTop - 10 + "px" : false;
+      break;
+
+    case 'ArrowDown':
+      player2.offsetTop < viewport.offsetHeight - player2.offsetHeight ? player2.style.top = player2.offsetTop + 10 + "px" : false;
+      break;
+
+    case 'ArrowLeft':
+      player2.offsetLeft > viewport.offsetLeft ? player2.style.left = player2.offsetLeft - 10 + "px" : false;
+      break;
+
+    case 'ArrowRight':
+      player2.offsetLeft < viewport.offsetWidth - player2.offsetWidth ? player2.style.left = player2.offsetLeft + 10 + "px" : false;
+      break;
+
+    default:
+      break;
+
+  }
+
+};
+
+// document.addEventListener('keydown', (event) => {
+//   movePlayerWithKeyboard(event);
+// });
+
+document.addEventListener('keydown', (event) => {
+  movePlayer2WithKeyboard(event);
+});
