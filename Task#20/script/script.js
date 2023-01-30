df63c9ad478cab5194577eb624d461bcb70285af
Curbat Anatolie <anatoliecurbat@gmail.com>
2023-01-30 20:22:45 +0200

    Task#20 code review

--- Task#20/script/script.js
+++ Task#20/script/script.js
@@ -1,20 +1,71 @@
+// const input = document.querySelector('input');
+// const addButton = document.querySelector('button');
+// const tbody = document.querySelector('tbody');
+
+// const addRowToTable = (tableBody) => {
+//   if (!tableBody) return;
+
+//   const row = document.createElement('tr');
+//   const td = document.createElement('td')
+//     .classList.add('username-td')
+//     .textContent = input.value;
+//   const removeButton = document.createElement('td')
+//     .classList.add('remove-td');
+//   const removeButtonIcon = document.createElement('i')
+//     .classList.add('fa-solid', 'fa-xmark');
+
+//   removeButton.append(removeButtonIcon);
+//   row.append(td);
+//   row.append(removeButton);
+//   tableBody.append(row);
+// };
+
+// tbody.addEventListener('click', (event) => {
+//   event.target.matches('.fa-xmark') ? event.target.closest('tr').remove() : false;
+// });
+
+// addButton.addEventListener('click', () => {
+//   if (input.value) {
+//     addRowToTable(tbody);
+//     input.value = '';
+//   }
+// });
+
+
+
+
 const input = document.querySelector('input');
 const addButton = document.querySelector('button');
 const tbody = document.querySelector('tbody');
 
+const addRowToTable = (tableBody) => {
+  if (!tableBody) return;
+
+  const row = document.createElement('tr');
+  const td = document.createElement('td');
+  td.classList.add('username-td');
+  td.textContent = input.value;
+
+  const removeButton = document.createElement('td');
+  removeButton.classList.add('remove-td');
+
+  const removeButtonIcon = document.createElement('i');
+  removeButtonIcon.classList.add('fa-solid', 'fa-xmark');
+
+  removeButton.append(removeButtonIcon);
+  row.append(td);
+  row.append(removeButton);
+  tableBody.append(row);
+};
+
+
 tbody.addEventListener('click', (event) => {
-  event.target.classList.contains('fa-xmark') ? event.target.parentNode.parentNode.remove() : false;
+  event.target.matches('.fa-xmark') ? event.target.parentNode.parentNode.remove() : false;
 });
 
 addButton.addEventListener('click', () => {
-  tbody.innerHTML += `
-  <tr>
-    <td style="width: 300px; height: 30px; padding-left: 10px; border: 1px solid black;">
-      ${input.value}
-    </td>
-    <td style="width: 30px; height: 35px; border: 1px solid black;" align="center">
-      <i class="fa-solid fa-xmark" style="color: red;"></i>
-    </td>
-  </tr>
-  `;
+  if (input.value) {
+    addRowToTable(tbody);
+    input.value = '';
+  }
 });
