b93e0a1f8089202ce4ce8531ec51ca9779aab304
Tolea2001 <anatoliecurbat@gmail.com>
2023-01-31 11:01:08 +0300

    Code review

--- src/7.js
+++ src/7.js
@@ -1,5 +1,5 @@
 const addYears = (date, year) => {
-    return new Date(date.setFullYear(date.getFullYear() + year));
+  return new Date(date.setFullYear(date.getFullYear() + year));
 };
 
 console.log(addYears(new Date(), 10).toString());
