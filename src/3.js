bade568971c6ec1df074a51ab03c46d44c8dccf4
Tolea2001 <anatoliecurbat@gmail.com>
2023-01-31 10:35:34 +0300

    Code review

--- src/3.js
+++ src/3.js
@@ -3,19 +3,19 @@ const b = 200;
 const c = 100;
 
 if (a > b && a > c) {
-    console.log('Numarul mai mare este ' + a);
+  console.log('Numarul mai mare este ' + a);
 } else if (b > a && b > c) {
-    console.log('Numarul mai mare este ' + b);
+  console.log('Numarul mai mare este ' + b);
 } else if (c > a && c > b) {
-    console.log('Numarul mai mare este ' + c);
+  console.log('Numarul mai mare este ' + c);
 }
 
 if (a < b && a < c) {
-    console.log('Numarul mai mic este ' + a);
+  console.log('Numarul mai mic este ' + a);
 } else if (b < a && b < c) {
-    console.log('Numarul mai mic este ' + b);
+  console.log('Numarul mai mic este ' + b);
 } else if (c < a && c < b) {
-    console.log('Numarul mai mic este ' + c);
+  console.log('Numarul mai mic este ' + c);
 }
 
 // console.log(`Numarul mai mare este: ${Math.max(a, b, c)}\nNumarul mai mic este: ${Math.min(a, b, c)}`);
