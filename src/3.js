b068f1ce0b067b8029aa49acc3a97b74bc4d070a
Curbat Anatolie <anatoliecurbat@gmail.com>
2023-02-08 11:44:44 +0200

    Tasak#3 layout

---
+++ src/3.js
@@ -0,0 +1,5 @@
+const inputName = document.querySelector('#input-name');
+const selectCity = document.querySelector('#inputState');
+const textarea = document.querySelector('#floatingTextarea2');
+const clearButton = document.querySelector('.btn-primary');
+const saveButton = document.querySelector('.btn-success');
