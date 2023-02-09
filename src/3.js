053c4d549a366128f6e31ad2a3ec03eeda380ca3
Curbat Anatolie <anatoliecurbat@gmail.com>
2023-02-09 20:03:12 +0200

    Task#3 without functional checkbox

--- src/3.js
+++ src/3.js
@@ -3,3 +3,37 @@ const selectCity = document.querySelector('#inputState');
 const textarea = document.querySelector('#floatingTextarea2');
 const clearButton = document.querySelector('.btn-primary');
 const saveButton = document.querySelector('.btn-success');
+
+const displayData = (value) => {
+  if (!localStorage.getItem('objectData')) return;
+
+  inputName.value = value.name;
+  textarea.value = value.message;
+
+  for (let i = 0; i < selectCity.options.length; i++) {
+    if (selectCity.options[i].value === value.selectCity) {
+      selectCity.selectedIndex = i;
+      break;
+    }
+  }
+};
+
+window.addEventListener('load', () => {
+  const localStorageObject = JSON.parse(localStorage.getItem('objectData'));
+  displayData(localStorageObject);
+});
+
+saveButton.addEventListener('click', () => {
+  const data = {
+    name: inputName.value,
+    selectCity: selectCity.value,
+    message: textarea.value,
+  };
+
+  localStorage.setItem('objectData', JSON.stringify(data));
+});
+
+clearButton.addEventListener('click', () => {
+  localStorage.clear();
+  location.reload();
+});
