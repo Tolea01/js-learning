7499606b5b808face1ca98082527b3d3d00e5d96
Curbat Anatolie <anatoliecurbat@gmail.com>
2023-02-11 02:08:26 +0200

    Task #3 - store Object in localStorage

--- src/3.js
+++ src/3.js
@@ -1,22 +1,10 @@
 const inputName = document.querySelector('#input-name');
 const selectCity = document.querySelector('#inputState');
+const checkboxes = document.querySelectorAll('.checkbox input[type="checkbox"]');
+const languages = [];
 const textarea = document.querySelector('#floatingTextarea2');
 const clearButton = document.querySelector('.btn-primary');
 const saveButton = document.querySelector('.btn-success');
-
-const displayData = (value) => {
-  if (!localStorage.getItem('objectData')) return;
-
-  inputName.value = value.name;
-  textarea.value = value.message;
-
-  for (let i = 0; i < selectCity.options.length; i++) {
-    if (selectCity.options[i].value === value.selectCity) {
-      selectCity.selectedIndex = i;
-      break;
-    }
-  }
-};
 
 window.addEventListener('load', () => {
   const localStorageObject = JSON.parse(localStorage.getItem('objectData'));
@@ -24,11 +12,16 @@ window.addEventListener('load', () => {
 });
 
 saveButton.addEventListener('click', () => {
+  if (localStorage.length) return;
+
   const data = {
     name: inputName.value,
     selectCity: selectCity.value,
+    languages,
     message: textarea.value,
   };
+
+  checkboxes.forEach((checkbox) => checkbox.checked ? languages.push(checkbox.id) : null);
 
   localStorage.setItem('objectData', JSON.stringify(data));
 });
@@ -37,3 +30,25 @@ clearButton.addEventListener('click', () => {
   localStorage.clear();
   location.reload();
 });
+
+const displayData = (value) => {
+  if (!localStorage.getItem('objectData')) return;
+
+  inputName.value = value.name;
+  textarea.value = value.message;
+
+  for (const i in selectCity) {
+    if (selectCity.options[i].value === value.selectCity) {
+      selectCity.selectedIndex = i;
+      break;
+    }
+  }
+
+  for (const checkbox of checkboxes) {
+    if (value.languages.includes(checkbox.id)) {
+      checkbox.checked = true;
+    } else {
+      checkbox.checked = false;
+    }
+  }
+};
