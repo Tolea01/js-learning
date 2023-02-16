9cee5ce447f4e41e190559c01b70ca23525dd133
Curbat Anatolie <anatoliecurbat@gmail.com>
2023-02-16 21:36:11 +0200

    Задача №2 Метод GET - получение информации

--- src/2.js
+++ src/2.js
@@ -1,47 +1,82 @@
-document.body.style = 'margin: 0';
+// document.body.style.cssText = 'margin: 0; width: 100%; display: flex; justify-content: center;';
 
-const CssBorder = 'border: 1px solid black';
-const table = document.body.appendChild(document.createElement('table'));
+// const CssBorder = 'border: 1px solid black';
+// const table = document.body.appendChild(document.createElement('table'));
 
 
-const createTableTemplate = (data) => {
-  const tr = document.createElement('tr');
+// const createTableTemplate = (data) => {
+//   const tr = document.createElement('tr');
 
-  for (const key of Object.keys(data[0])) {
-    const td = document.createElement('td');
+//   for (const key of Object.keys(data[0])) {
+//     const td = document.createElement('td');
 
-    td.textContent = key;
-    td.style = CssBorder;
+//     td.textContent = key.toUpperCase();
+//     td.style = CssBorder;
 
-    tr.appendChild(td);
-    table.appendChild(tr);
-  }
+//     tr.appendChild(td);
+//     table.appendChild(tr);
+//   }
 
-  table.style.cssText = `width: 100vw; text-align: center; margin-top: 30px; ${CssBorder}`;
-};
+//   table.style.cssText = `width: 80%; text-align: center; margin-top: 30px; ${CssBorder}`;
+// };
 
-const displayData = (data) => {
-  for (const task of data) {
+// const displayData = (data) => {
+//   for (const task of data) {
+//     const tr = document.createElement('tr');
+
+//     if (typeof task === 'object') {
+//       for (const taskProperty of Object.values(task)) {
+//         const td = document.createElement('td');
+//         td.textContent = taskProperty;
+//         td.style = CssBorder;
+
+//         tr.appendChild(td);
+//       }
+
+//       table.appendChild(tr);
+//     }
+//   }
+// }
+
+// fetch('https://jsonplaceholder.typicode.com/todos')
+//   .then((data) => data.json())
+//   .then((data) => {
+//     createTableTemplate(data);
+//     displayData(data);
+//   })
+//   .catch((error) => console.error(error));
+
+// ---------------------------------------------------------------------------------------------------------------------------------------
+
+document.body.style.cssText = 'width: 100%; display: flex; justify-content: center';
+
+const getDataById = (data, id) => {
+  const taskObject = data.find((elem) => elem.id === id)
+  const table = document.body.appendChild(document.createElement('table'));
+  table.style.cssText = 'width: 80%; border: 1px solid black; text-align: center';
+
+
+  const insertTaskInTable = (objectProperty) => {
     const tr = document.createElement('tr');
 
-    if (typeof task === 'object') {
-      for (const taskProperty of Object.values(task)) {
-        const td = document.createElement('td');
-        td.textContent = taskProperty;
-        td.style = CssBorder;
+    for (const data of objectProperty) {
+      const td = document.createElement('td');
+      td.textContent = data;
+      td.style.border = '1px solid black';
 
-        tr.appendChild(td);
-      }
-
-      table.appendChild(tr);
+      tr.appendChild(td);
     }
-  }
+
+    table.appendChild(tr);
+  };
+
+  insertTaskInTable(Object.keys(taskObject));
+  insertTaskInTable(Object.values(taskObject));
+
+  table.firstChild.style.textTransform = 'uppercase';
 }
 
 fetch('https://jsonplaceholder.typicode.com/todos')
   .then((data) => data.json())
-  .then((data) => {
-    createTableTemplate(data);
-    displayData(data);
-  })
-  .catch((error) => console.error(error));
+  .then((data) => getDataById(data, 150))
+  .catch((err) => console.error(err));
