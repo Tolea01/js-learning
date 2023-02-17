0f9db7d359bfbb967bb4c29c45bd2c86a725a24f
Tolea2001 <anatoliecurbat@gmail.com>
2023-02-18 00:31:07 +0200

    Задача №3 Метод POST - добавление информации

--- src/3.js
+++ src/3.js
@@ -1,11 +1,36 @@
+const button = document.querySelector('button');
+
 const data = {
-  "title": "8888888",
-  "body": "688888",
-  "execution_time": "5645"
+  title: 8888888,
+  body: 688888,
+  execution_time: 5645,
 };
 
-fetch('https://jsonplaceholder.typicode.com/todos', {
-  method: 'POST',
-  body: JSON.stringify(data),
+
+const sendData = async (url, data) => {
+  const response = await fetch(url, {
+    method: 'POST',
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": "application/json"
+    }
+  });
+
+  if (!response.ok) {
+    throw new Error('Server error');
+  }
+
+  alert('The data has been sent to the server!');
+};
+
+button.addEventListener('click', () => {
+  sendData('http://localhost:3000/todos', data);
+  // fetch('http://localhost:3000/todos', {
+  //   method: 'POST',
+  //   body: JSON.stringify(data),
+  //   headers: {
+  //     "Content-Type": "application/json"
+  //   }
+  // })
+  //   .catch((err) => console.error(err));
 })
-.catch((err) => console.error(err));
