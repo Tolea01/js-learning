05293f09e18b5cbdceb8c89453f4f47db677d004
Tolea2001 <anatoliecurbat@gmail.com>
2023-05-24 03:05:57 +0300

    Task #2 - OOP basics

--- src/1.js
+++ src/1.js
@@ -3,8 +3,24 @@ class Person {
     this.name = name;
     this.surname = surname;
   }
+
+  getName() {
+    return this.name;
+  }
+
+  getSurname() {
+    return this.surname;
+  }
+
+  setSurname(newSurname) {
+    this.surname = newSurname;
+    return 'Ivanov';
+  }
 }
 
 const worker = new Person('Ivan', 'Ivanov');
-console.log(worker.name);
-console.log(worker.surname);
+
+console.log(worker.getName());
+console.log(worker.getSurname());
+console.log(worker.setSurname('Petrov'));
+console.log(worker.getSurname());
